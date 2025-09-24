914756dda4f89824d404cfdecabe06132bafb72d
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-09-24 17:01:32 +1200

    Update start with normal installation

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@
 # map utilities
 cd ./map-utilities || exit
 echo 'Updating map-utilities ...'
-npm ci
+npm install
 npm run build-bundle
 cd - || exit
 
 # Navigate to map-sidebar directory, link map-utilities, and exit
 cd ./map-sidebar || exit
 echo 'Updating map-sidebar ...'
-npm ci
+npm install
 npm link ../map-utilities
 npm run build-bundle
 cd - || exit
@@ -19,7 +19,7 @@ cd - || exit
 # Navigate to flatmapvuer directory, link map-utilities, and exit
 cd ./flatmapvuer || exit
 echo 'Updating flatmapvuer ...'
-npm ci
+npm install
 npm link ../map-utilities
 npm run build-bundle
 cd - || exit
@@ -27,7 +27,7 @@ cd - || exit
 # Navigate to scaffoldvuer directory, link map-utilities, and exit
 cd ./scaffoldvuer || exit
 echo 'Updating scaffoldvuer ...'
-npm ci
+npm install
 npm link ../map-utilities
 npm run build-bundle
 cd - || exit
@@ -35,7 +35,7 @@ cd - || exit
 # Navigate to mapintegratedvuer directory, link map-utilities, map-sidebar, and flatmapvuer, and exit
 cd ./mapintegratedvuer || exit
 echo 'Updating mapintegratedvuer ...'
-npm ci
+npm install
 npm link ../map-utilities ../map-sidebar ../flatmapvuer ../scaffoldvuer
 npm run serve
 cd - || exit
